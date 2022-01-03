97dac9055c13b68f82c9383effcc7f8b6460619b
Kévin <kevindubuc62@gmail.com>
2022-01-03 17:43:05 +0100

    bump php-cs-fixe to v3

--- php-cs-fixer.php
+++ php-cs-fixer.php
@@ -8,43 +8,43 @@ $finder = Finder::create()->in(getcwd());
 
 // Set of rules
 $rules = [
-    'psr0'                   => true,
+    'psr_autoloading'        => true,
     '@PSR1'                  => true,
     '@PSR2'                  => true,
     '@Symfony'               => true,
     '@Symfony:risky'         => true,
     '@PHP71Migration'        => true,
     'binary_operator_spaces' => [
-        'align_double_arrow' => true,
-        'align_equals'       => true,
+        'operators' => [
+            '=>' => 'align',
+            '='  => 'align',
+        ],
     ],
     'array_syntax' => [
         'syntax' => 'short',
     ],
     'combine_consecutive_unsets'       => true,
     'general_phpdoc_annotation_remove' => [
-        'param',
-        'return',
+        'annotations' => ['param', 'return'],
     ],
-    'is_null' => [
-        'use_yoda_style' => true,
-    ],
+    'is_null'                     => true,
+    'yoda_style'                  => true,
     'linebreak_after_opening_tag' => true,
     'list_syntax'                 => [
         'syntax' => 'short',
     ],
     'mb_str_functions' => true,
     'ordered_imports'  => [
-        'sortAlgorithm' => 'length',
+        'sort_algorithm' => 'length',
     ],
     'return_type_declaration' => [
         'space_before' => 'one',
     ],
     'semicolon_after_instruction' => true,
-    'silenced_deprecation_error'  => false,
+    'error_suppression'           => false,
 ];
 
-return Config::create()
+return (new Config())
     ->setFinder($finder)
     ->setRules($rules)
     ->setRiskyAllowed(true);
